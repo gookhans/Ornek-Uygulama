3ae69c55c9c88edae1aa68f5f9499c88a613a385
gookhans <gookhansancars@gmail.com>
2018-04-25 17:43:20 +0300

    ilk kayit

---
+++ WpfApplication2/Register.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OrnekUygulama
+{
+ 
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        
+        public Register()
+        {
+            InitializeComponent();
+            guvenlikkodu();
+        }
+        
+        public void guvenlikkodu()
+        {
+            
+           String kod = "";
+            int hangisi,harf,sayac;
+            Random Rharf = new Random();
+            Random Rrakam = new Random();
+            Random Rhangisi = new Random();
+            for(int i=0; i<5; i++)
+            {
+                sayac = 0;
+                hangisi = Rhangisi.Next(1, 3);
+                if (hangisi == 1)
+                    kod += Rrakam.Next(0, 10).ToString();
+               else if (hangisi == 2)
+                {
+                    
+                    harf = Rharf.Next(1, 27);
+                    for (char h = 'a'; h <= 'z'; h++)
+                    {
+                        sayac++;
+                        if (harf == sayac)
+                         kod += h;
+                            
+                        
+                    }
+                }
+
+            }
+            GuvenlikKod.Content = kod;
+        }
+    }
+}
